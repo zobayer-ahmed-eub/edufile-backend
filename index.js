89514dd14de937c9d6b57a793cdcba108b3dd342
Zobayer Ahmed <zobayer@eub.edu.bd>
2026-06-15 05:20:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ app.post('/api/submitForm', async (req, res) => {
     const table = data.database_table || "contact_inquiries";
 
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            ssl: { rejectUnauthorized: false }
-        });
+       const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
         // Insert Logic
         if (table === 'admission_requests') {
